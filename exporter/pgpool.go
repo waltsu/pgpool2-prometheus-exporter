4bce1e7cf692385ed971213fe5d35e7723b9e934
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2018-12-24 11:31:41 +0200

    Give pcp default parameters

--- exporter/pgpool.go
+++ exporter/pgpool.go
@@ -4,8 +4,14 @@ import (
 	"log"
 )
 
-const (
-	PcpLocation = "/usr/sbin/" // TODO: Configure from env variables
+var (
+	PcpLocation = GetEnv("PCP_LOCATION", "/usr/sbin/")
+
+	PcpUsername = GetEnv("PCP_USERNAME", "pcpuser")
+	PcpHost = GetEnv("PCP_HOST", "localhost")
+	PcpPort = GetEnv("PCP_PORT", "9898")
+
+	PcpDefaultArguments = []string{ "-w", "-U " + PcpUsername, "-h " + PcpHost, "-p " + PcpPort }
 )
 
 type PgPool struct {
@@ -18,7 +24,7 @@ func NewPgPool(executor CommandExecutor) *PgPool {
 }
 
 func (pgpool *PgPool) GetNodeCount() (int, error) {
-	response, err := pgpool.commandExecutor.Execute(PcpLocation + "pcp_node_count")
+	response, err := pgpool.commandExecutor.Execute(PcpLocation + "pcp_node_count", PcpDefaultArguments...)
 	if err != nil {
 		return -1, err
 	}
